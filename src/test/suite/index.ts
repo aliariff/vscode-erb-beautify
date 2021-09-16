24688d616e2e970c36c7f91bd3ff89a6be8c492c
Ali Ariff <ali.ariff12@gmail.com>
2021-09-17 01:53:29 +0200

    fix: increase test timeout

--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -7,7 +7,7 @@ export function run(): Promise<void> {
   const mocha = new Mocha({
     ui: "tdd",
     color: true,
-    timeout: "5000",
+    timeout: "10000",
   });
 
   const testsRoot = path.resolve(__dirname, "..");
