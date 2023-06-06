d180f3b64594fbf02a1c6ab2a56d4e01e5e014f3
Ali Ariff <ali.ariff12@gmail.com>
2023-06-06 20:29:48 +0200

    build: Use test-electron lib

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -1,6 +1,6 @@
 import * as path from "path";
 
-import { runTests } from "vscode-test";
+import { runTests } from "@vscode/test-electron";
 
 async function main() {
   try {
@@ -8,7 +8,7 @@ async function main() {
     // Passed to `--extensionDevelopmentPath`
     const extensionDevelopmentPath = path.resolve(__dirname, "../../");
 
-    // The path to test runner
+    // The path to the extension test script
     // Passed to --extensionTestsPath
     const extensionTestsPath = path.resolve(__dirname, "./suite/index");
 
@@ -16,16 +16,6 @@ async function main() {
     await runTests({
       extensionDevelopmentPath,
       extensionTestsPath,
-      /**
-       * A list of launch arguments passed to VS Code executable, in addition to `--extensionDevelopmentPath`
-       * and `--extensionTestsPath` which are provided by `extensionDevelopmentPath` and `extensionTestsPath`
-       * options.
-       *
-       * If the first argument is a path to a file/folder/workspace, the launched VS Code instance
-       * will open it.
-       *
-       * See `code --help` for possible arguments.
-       */
       launchArgs: ["--disable-extensions"],
     });
   } catch (err) {
