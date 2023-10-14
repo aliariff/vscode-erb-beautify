dde9067f83706bae5d8c18202acc1032a90a2907
Ali Ariff <ali.ariff@accurids.com>
2023-10-14 11:01:47 +0200

    test: refactor and add test files structure

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,103 +1,28 @@
 import * as assert from "assert";
-
 import * as vscode from "vscode";
+import * as fs from "fs";
+import * as path from "path";
 
 suite("ERB Formatter/Beautify tests", () => {
-  const FIXTURE = `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-      <html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
-      <head>
-      <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-      <script src="/javascript/prototype.js" type="text/javascript"></script>
-      <link rel="stylesheet" type="text/css" href="/stylesheets/screen.css" media="screen"/>
-      <!--[if IE 6]>
-      <link rel="stylesheet" href="/stylesheets/screen_ie6.css" type="text/css" />
-      <![endif]-->
-      <title>Title Goes Here</title>
-      <script type="text/javascript" charset="utf-8">
-      doSomething();
-      </script>
-      </head>
-      <body>
-      <div id="something">
-      <h1>
-      Heading 1
-      </h1>
-      </div>
-      <div id="somethingElse"><p>Lorem Ipsum</p>
-      <% if @x %>
-      <% @ys.each do |y| %>
-      <p>
-      <%= h y %>
-      </p>
-      <% end %>
-      <% elsif @z %>
-      <hr />
-      <% end %>
-      </div>
-      <table>
-      <colgroup>
-      <col style="width: 50%;">
-      <col style="width: 50%;">
-      </colgroup>
-      <tbody>
-      <tr><td>First column</td></tr><tr>
-      <td>Second column</td></tr>
-      </tbody>
-      </table>
-      </body>
-      </html>`;
-  const CORRECT = `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
-  <head>
-    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-    <script src="/javascript/prototype.js" type="text/javascript"></script>
-    <link rel="stylesheet" type="text/css" href="/stylesheets/screen.css" media="screen"/>
-    <!--[if IE 6]>
-      <link rel="stylesheet" href="/stylesheets/screen_ie6.css" type="text/css" />
-    <![endif]-->
-    <title>Title Goes Here</title>
-    <script type="text/javascript" charset="utf-8">
-      doSomething();
-    </script>
-  </head>
-  <body>
-    <div id="something">
-      <h1>
-        Heading 1
-      </h1>
-    </div>
-    <div id="somethingElse">
-      <p>Lorem Ipsum</p>
-      <% if @x %>
-        <% @ys.each do |y| %>
-          <p>
-            <%= h y %>
-          </p>
-        <% end %>
-      <% elsif @z %>
-        <hr />
-      <% end %>
-    </div>
-    <table>
-      <colgroup>
-        <col style="width: 50%;">
-        <col style="width: 50%;">
-      </colgroup>
-      <tbody>
-        <tr>
-          <td>First column</td>
-        </tr>
-        <tr>
-          <td>Second column</td>
-        </tr>
-      </tbody>
-    </table>
-  </body>
-</html>
-`;
-
   const sleep = (ms: number) =>
     new Promise((resolve) => setTimeout(resolve, ms));
+
+  /**
+   * Reads the content of a test file located in the `data` directory.
+   *
+   * @param {string} filename - The name of the file to be read.
+   * @returns {string} - The content of the file in UTF-8 encoding.
+   * @throws {Error} - Throws an error if the file cannot be read.
+   */
+  function readTestFile(filename: string): string {
+    const testFilePath = path.resolve(
+      __dirname,
+      "../../../",
+      "src/test/fixtures/",
+      filename
+    );
+    return fs.readFileSync(testFilePath, "utf-8");
+  }
 
   /**
    * Changes the configuration value
@@ -118,13 +43,16 @@ suite("ERB Formatter/Beautify tests", () => {
   async function formatWholeDocument(): Promise<void> {
     const document = await vscode.workspace.openTextDocument({
       language: "erb",
-      content: FIXTURE,
+      content: readTestFile("sample_1_unformatted.html.erb"),
     });
     await vscode.window.showTextDocument(document);
     await sleep(1500); // we need to wait a little bit until extension is loaded
     await vscode.commands.executeCommand("editor.action.formatDocument");
     await sleep(500); // wait until extension executed
-    assert.strictEqual(document.getText(), CORRECT);
+    assert.strictEqual(
+      document.getText(),
+      readTestFile("sample_1_formatted.html.erb")
+    );
   }
 
   /**
@@ -134,14 +62,17 @@ suite("ERB Formatter/Beautify tests", () => {
   async function formatSelectionDocument(): Promise<void> {
     const document = await vscode.workspace.openTextDocument({
       language: "erb",
-      content: FIXTURE,
+      content: readTestFile("sample_1_unformatted.html.erb"),
     });
     await vscode.window.showTextDocument(document);
     await vscode.commands.executeCommand("editor.action.selectAll");
     await sleep(1500); // we need to wait a little bit until extension is loaded
     await vscode.commands.executeCommand("editor.action.formatSelection");
     await sleep(500); // wait until extension executed
-    assert.strictEqual(document.getText(), CORRECT);
+    assert.strictEqual(
+      document.getText(),
+      readTestFile("sample_1_formatted.html.erb")
+    );
   }
 
   test("formats whole document without bundler", async () => {
