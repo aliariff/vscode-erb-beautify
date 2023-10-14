947b9e26361de856866fb394cb6a7862e8ad491a
Ali Ariff <ali.ariff@accurids.com>
2023-10-14 11:18:57 +0200

    test: more refactor & add encoding test

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -8,27 +8,27 @@ suite("ERB Formatter/Beautify tests", () => {
     new Promise((resolve) => setTimeout(resolve, ms));
 
   /**
-   * Reads the content of a test file located in the `fixtures` directory.
-   *
-   * @param {string} filename - The name of the file to be read.
-   * @returns {string} - The content of the file in UTF-8 encoding.
-   * @throws {Error} - Throws an error if the file cannot be read.
+   * Resolve the test file path located in the `fixtures` directory.
+   * @param filename - The name of the file.
+   * @returns The full path of the test file.
    */
-  function readTestFile(filename: string): string {
-    const testFilePath = path.resolve(
-      __dirname,
-      "../../../",
-      "src/test/fixtures/",
-      filename
-    );
-    return fs.readFileSync(testFilePath, "utf-8");
+  function resolveTestFilePath(filename: string): string {
+    return path.resolve(__dirname, "../../../", "src/test/fixtures/", filename);
   }
 
   /**
-   * Changes the configuration value
-   * @param {string} key - The configuration key
-   * @param {any} value - The new value
-   * @returns {Promise<void>}
+   * Reads the content of a test file located in the `fixtures` directory.
+   * @param filename - The name of the file to be read.
+   * @returns The content of the file in UTF-8 encoding.
+   */
+  function readTestFile(filename: string): string {
+    return fs.readFileSync(resolveTestFilePath(filename), "utf-8");
+  }
+
+  /**
+   * Changes a specific configuration value.
+   * @param key - The configuration key.
+   * @param value - The new value to set.
    */
   async function changeConfig(key: string, value: any): Promise<void> {
     await vscode.workspace
@@ -37,61 +37,67 @@ suite("ERB Formatter/Beautify tests", () => {
   }
 
   /**
-   * Formats the whole document and asserts the result
-   * @returns {Promise<void>}
+   * Formats a document and asserts its content against an expected formatted version.
+   * @param initialFile - The initial unformatted file name.
+   * @param expectedFile - The expected formatted file name.
+   * @param formatCommand - The vscode command to execute for formatting.
    */
-  async function formatWholeDocument(): Promise<void> {
+  async function formatAndAssert(
+    initialFile: string,
+    expectedFile: string,
+    formatCommand: string = "editor.action.formatDocument"
+  ): Promise<void> {
     const document = await vscode.workspace.openTextDocument({
       language: "erb",
-      content: readTestFile("sample_1_unformatted.html.erb"),
+      content: readTestFile(initialFile),
     });
-    await vscode.window.showTextDocument(document);
-    await sleep(1500); // we need to wait a little bit until extension is loaded
-    await vscode.commands.executeCommand("editor.action.formatDocument");
-    await sleep(500); // wait until extension executed
-    assert.strictEqual(
-      document.getText(),
-      readTestFile("sample_1_formatted.html.erb")
-    );
-  }
 
-  /**
-   * Formats the selected range in the document and asserts the result
-   * @returns {Promise<void>}
-   */
-  async function formatSelectionDocument(): Promise<void> {
-    const document = await vscode.workspace.openTextDocument({
-      language: "erb",
-      content: readTestFile("sample_1_unformatted.html.erb"),
-    });
     await vscode.window.showTextDocument(document);
-    await vscode.commands.executeCommand("editor.action.selectAll");
-    await sleep(1500); // we need to wait a little bit until extension is loaded
-    await vscode.commands.executeCommand("editor.action.formatSelection");
-    await sleep(500); // wait until extension executed
-    assert.strictEqual(
-      document.getText(),
-      readTestFile("sample_1_formatted.html.erb")
-    );
+    await sleep(1500); // Allow time for the extension to load.
+    await vscode.commands.executeCommand(formatCommand);
+    await sleep(500); // Allow time for the formatting to occur.
+
+    assert.strictEqual(document.getText(), readTestFile(expectedFile));
   }
 
   test("formats whole document without bundler", async () => {
     await changeConfig("vscode-erb-beautify.useBundler", false);
-    await formatWholeDocument();
+    await formatAndAssert(
+      "sample_unformatted.html.erb",
+      "sample_formatted.html.erb"
+    );
   });
 
   test("formats whole document using bundler", async () => {
     await changeConfig("vscode-erb-beautify.useBundler", true);
-    await formatWholeDocument();
+    await formatAndAssert(
+      "sample_unformatted.html.erb",
+      "sample_formatted.html.erb"
+    );
   });
 
   test("formats selection without bundler", async () => {
     await changeConfig("vscode-erb-beautify.useBundler", false);
-    await formatSelectionDocument();
+    await formatAndAssert(
+      "sample_unformatted.html.erb",
+      "sample_formatted.html.erb",
+      "editor.action.formatSelection"
+    );
   });
 
   test("formats selection using bundler", async () => {
     await changeConfig("vscode-erb-beautify.useBundler", true);
-    await formatSelectionDocument();
+    await formatAndAssert(
+      "sample_unformatted.html.erb",
+      "sample_formatted.html.erb",
+      "editor.action.formatSelection"
+    );
+  });
+
+  test("formats without encoding issue", async () => {
+    await formatAndAssert(
+      "encoding_unformatted.html.erb",
+      "encoding_formatted.html.erb"
+    );
   });
 });
