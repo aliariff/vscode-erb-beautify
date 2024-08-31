386f1bedff21fd5d5d875d467f30f0d457738b3c
Ali Ariff <ali.ariff@accurids.com>
2024-08-31 10:27:05 +0200

    feat: add config to ignore formatting

--- src/formatter/htmlbeautifierProvider.ts
+++ src/formatter/htmlbeautifierProvider.ts
@@ -1,5 +1,6 @@
 import * as vscode from "vscode";
 import HtmlBeautifier from "./htmlbeautifier";
+import micromatch from "micromatch";
 
 export default class HtmlBeautifierProvider
   implements
@@ -13,40 +14,30 @@ export default class HtmlBeautifierProvider
   }
 
   /**
-   * Provides formatting edits for the entire document
-   * @param {vscode.TextDocument} document - The document to be formatted
-   * @param {vscode.FormattingOptions} options - The formatting options
-   * @param {vscode.CancellationToken} token - The cancellation token
-   * @returns {vscode.ProviderResult<vscode.TextEdit[]>} The formatting edits
+   * Provides formatting edits for the entire document.
+   * @param document - The document to be formatted.
+   * @param options - The formatting options.
+   * @param token - The cancellation token.
+   * @returns The formatting edits.
    */
   public provideDocumentFormattingEdits(
     document: vscode.TextDocument,
     options: vscode.FormattingOptions,
     token: vscode.CancellationToken
   ): vscode.ProviderResult<vscode.TextEdit[]> {
-    return this.htmlbeautifier.format(document.getText()).then(
-      (result) => {
-        return [
-          new vscode.TextEdit(
-            document.validateRange(new vscode.Range(0, 0, Infinity, Infinity)),
-            result
-          ),
-        ];
-      },
-      (err) => {
-        // will be handled in format
-        return [];
-      }
+    return this.formatDocument(
+      document,
+      new vscode.Range(0, 0, Infinity, Infinity)
     );
   }
 
   /**
-   * Provides formatting edits for a specific range within the document
-   * @param {vscode.TextDocument} document - The document to be formatted
-   * @param {vscode.Range} range - The range to be formatted
-   * @param {vscode.FormattingOptions} options - The formatting options
-   * @param {vscode.CancellationToken} token - The cancellation token
-   * @returns {vscode.ProviderResult<vscode.TextEdit[]>} The formatting edits
+   * Provides formatting edits for a specific range within the document.
+   * @param document - The document to be formatted.
+   * @param range - The range to be formatted.
+   * @param options - The formatting options.
+   * @param token - The cancellation token.
+   * @returns The formatting edits.
    */
   public provideDocumentRangeFormattingEdits(
     document: vscode.TextDocument,
@@ -54,14 +45,41 @@ export default class HtmlBeautifierProvider
     options: vscode.FormattingOptions,
     token: vscode.CancellationToken
   ): vscode.ProviderResult<vscode.TextEdit[]> {
+    return this.formatDocument(document, range);
+  }
+
+  /**
+   * Formats the document or a specific range within the document.
+   * @param document - The document to be formatted.
+   * @param range - The range to be formatted.
+   * @returns The formatting edits.
+   */
+  private formatDocument(
+    document: vscode.TextDocument,
+    range: vscode.Range
+  ): vscode.ProviderResult<vscode.TextEdit[]> {
+    if (this.shouldIgnore(document)) {
+      console.log(`Ignoring ${document.fileName}`);
+      return [];
+    }
+
     return this.htmlbeautifier.format(document.getText(range)).then(
-      (result) => {
-        return [new vscode.TextEdit(range, result)];
-      },
+      (result) => [new vscode.TextEdit(range, result)],
       (err) => {
-        // will be handled in format
+        console.error(`Error formatting ${document.fileName}:`, err);
         return [];
       }
     );
+  }
+
+  /**
+   * Checks if the document should be ignored based on user-defined patterns.
+   * @param document - The document to be checked.
+   * @returns Whether the document should be ignored.
+   */
+  private shouldIgnore(document: vscode.TextDocument): boolean {
+    const config = vscode.workspace.getConfiguration("vscode-erb-beautify");
+    const ignorePatterns: string[] = config.get("ignoreFormatFilePatterns", []);
+    return micromatch.isMatch(document.fileName, ignorePatterns);
   }
 }
